0ac7a0a5f1d2838113b94968b787dfcb6a7fa4e0
limprove <limprove89@gmail.com>
2021-01-20 12:16:46 +0900

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  plugins: [
-    // eslint-disable-next-line global-require
-    require('tailwindcss'),
-
-    // eslint-disable-next-line global-require
-    require('autoprefixer'),
-  ],
+  plugins: ['tailwindcss', 'autoprefixer'],
 };
