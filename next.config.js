2fb09f775e20ac3e8a989bdb3d36e644ef91f618
limprove <limprove89@gmail.com>
2021-01-20 11:27:22 +0900

    bug fix!

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 const withCSS = require('@zeit/next-css');
 
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = withCSS({
   env: {
     CONTENT_API_KEY: '7b7f56ef7ce29367627b12e73b',
     BLOG_URL: 'https://next-ghost-blog-app.herokuapp.com',
-    APP_URL: 'https://next-ghost-blog-app.vercel.app',
+    APP_URL: prod ? 'https://next-ghost-blog-app.vercel.app' : 'http://localhost:3000',
   },
   compress: true,
   poweredByHeader: false,
   webpack(config) {
-    const prod = process.env.NODE_ENV === 'production';
+    // const prod = process.env.NODE_ENV === 'production';
     return {
       ...config,
       mode: prod ? 'production' : 'development',
